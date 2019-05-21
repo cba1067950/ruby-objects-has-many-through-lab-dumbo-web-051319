57458130ca9648991019203f07560815c68e3318
Joe Jamsky <cba1067950@yahoo.com>
2019-05-21 22:28:34 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -14,7 +14,7 @@ class Doctor
     @@all << self
   end 
   
-  def new_appointment(date, patient)
+  def new_appointment(patient, date)
     binding.pry
     Appointment.new(patient, date, self)
   end 
